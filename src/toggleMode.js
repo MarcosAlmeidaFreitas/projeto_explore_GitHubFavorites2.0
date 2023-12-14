8232fa8ccc8b8088f6b3ebbfad4b4a4199a90455
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-12-14 14:15:28 -0300

    alterações no toggleMode

--- src/toggleMode.js
+++ src/toggleMode.js
@@ -1,7 +1,13 @@
 const buttonToggle = document.getElementById('toggleButton');
-
+const spanToggle = document.querySelector('#spanToggleButton');
+let lightMode = false;
 
 buttonToggle.addEventListener('click', (event)=>{
+  lightMode = !lightMode;
+  
+  if (lightMode) spanToggle.textContent = 'Ativar LightMode';
+  else spanToggle.textContent = 'Ativar DarkMode';
+
   document.documentElement.classList.toggle('light');
 });
 
